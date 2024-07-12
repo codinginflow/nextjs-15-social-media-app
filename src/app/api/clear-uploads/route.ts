a71236405d859316b88f5151e2bc34794652dd00
Coding in Flow <info@codinginflow.com>
2024-07-12 10:54:50 +0200

    End of part 23

---
+++ src/app/api/clear-uploads/route.ts
@@ -0,0 +1,52 @@
+import prisma from "@/lib/prisma";
+import { UTApi } from "uploadthing/server";
+
+export async function GET(req: Request) {
+  try {
+    const authHeader = req.headers.get("Authorization");
+
+    if (authHeader !== `Bearer ${process.env.CRON_SECRET}`) {
+      return Response.json(
+        { message: "Invalid authorization header" },
+        { status: 401 },
+      );
+    }
+
+    const unusedMedia = await prisma.media.findMany({
+      where: {
+        postId: null,
+        ...(process.env.NODE_ENV === "production"
+          ? {
+              createdAt: {
+                lte: new Date(Date.now() - 1000 * 60 * 60 * 24),
+              },
+            }
+          : {}),
+      },
+      select: {
+        id: true,
+        url: true,
+      },
+    });
+
+    new UTApi().deleteFiles(
+      unusedMedia.map(
+        (m) =>
+          m.url.split(`/a/${process.env.NEXT_PUBLIC_UPLOADTHING_APP_ID}/`)[1],
+      ),
+    );
+
+    await prisma.media.deleteMany({
+      where: {
+        id: {
+          in: unusedMedia.map((m) => m.id),
+        },
+      },
+    });
+
+    return new Response();
+  } catch (error) {
+    console.error(error);
+    return Response.json({ error: "Internal server error" }, { status: 500 });
+  }
+}
